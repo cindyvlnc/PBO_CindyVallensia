afa5e02f71bb33fc3287826650b71c0915193dc9
cindyvlnc <116186314+cindyvlnc@users.noreply.github.com>
2023-01-17 00:46:54 +0700

    Add files via upload

---
+++ src/CindyVallensia_modul3/cindy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CindyVallensia_modul3;
+
+/**
+ *
+ * @author satri
+ */
+public class cindy {
+     public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming Satria Rhama !!!");
+    }
+}
